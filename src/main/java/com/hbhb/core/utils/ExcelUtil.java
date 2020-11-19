dd8b5d42af862ad297cf4bf74129fd881e9088f5
xiaokang <648638730@qq.com>
2020-11-19 21:39:42 +0800

    工具类方法新增

--- src/main/java/com/hbhb/core/utils/ExcelUtil.java
+++ src/main/java/com/hbhb/core/utils/ExcelUtil.java
@@ -2,6 +2,8 @@ package com.hbhb.core.utils;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 
 import org.springframework.util.StringUtils;
 
@@ -23,6 +25,20 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings(value = {"rawtypes"})
 public class ExcelUtil {
 
+    private static final HorizontalCellStyleStrategy HORIZONTAL_CELL_STYLE_STRATEGY;
+
+    static {
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 单元格自动换行
+        contentWriteCellStyle.setWrapped(Boolean.TRUE);
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HORIZONTAL_CELL_STYLE_STRATEGY =
+                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+    }
+
     public static void export2Web(HttpServletResponse response,
                                   String fileName,
                                   String sheetName,
@@ -33,7 +49,10 @@ public class ExcelUtil {
             response.setContentType("application/vnd.ms-excel");
             response.setCharacterEncoding("utf-8");
             response.setHeader("Content-disposition", "attachment;filename=" + fileName);
-            EasyExcel.write(response.getOutputStream(), clazz).sheet(sheetName).doWrite(data);
+            EasyExcel.write(response.getOutputStream(), clazz)
+                    .sheet(sheetName)
+                    .registerWriteHandler(HORIZONTAL_CELL_STYLE_STRATEGY)
+                    .doWrite(data);
         } catch (Exception e) {
             log.error("导出Excel异常{}", e.getMessage());
             throw new RuntimeException("导出Excel失败，请联系网站管理员！");
@@ -50,14 +69,16 @@ public class ExcelUtil {
             response.setContentType("application/vnd.ms-excel");
             response.setCharacterEncoding("utf-8");
             response.setHeader("Content-disposition", "attachment;filename=" + fileName);
-            EasyExcel.write(response.getOutputStream()).withTemplate(templateFileName)
-                    .sheet(sheetName).doWrite(data);
+            EasyExcel.write(response.getOutputStream())
+                    .withTemplate(templateFileName)
+                    .sheet(sheetName)
+                    .registerWriteHandler(HORIZONTAL_CELL_STYLE_STRATEGY)
+                    .doWrite(data);
         } catch (Exception e) {
             log.error("导出Excel异常{}", e.getMessage());
             throw new RuntimeException("导出Excel失败，请联系网站管理员！");
         }
     }
-
 
     public static String encodingFileName(HttpServletRequest request, String fileName) {
         final String userAgent = request.getHeader("user-agent");
