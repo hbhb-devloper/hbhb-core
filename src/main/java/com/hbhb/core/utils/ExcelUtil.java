d8e4c4fed65dcd440d9a59c2b2d7a86e2a2a7f02
xiaokang <648638730@qq.com>
2020-11-19 21:52:07 +0800

    优化代码

--- src/main/java/com/hbhb/core/utils/ExcelUtil.java
+++ src/main/java/com/hbhb/core/utils/ExcelUtil.java
@@ -2,8 +2,6 @@ package com.hbhb.core.utils;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.support.ExcelTypeEnum;
-import com.alibaba.excel.write.metadata.style.WriteCellStyle;
-import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 
 import org.springframework.util.StringUtils;
 
@@ -25,20 +23,6 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings(value = {"rawtypes"})
 public class ExcelUtil {
 
-    private static final HorizontalCellStyleStrategy HORIZONTAL_CELL_STYLE_STRATEGY;
-
-    static {
-        // 头的策略
-        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
-        // 内容的策略
-        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
-        // 单元格自动换行
-        contentWriteCellStyle.setWrapped(Boolean.TRUE);
-        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
-        HORIZONTAL_CELL_STYLE_STRATEGY =
-                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
-    }
-
     public static void export2Web(HttpServletResponse response,
                                   String fileName,
                                   String sheetName,
@@ -51,7 +35,6 @@ public class ExcelUtil {
             response.setHeader("Content-disposition", "attachment;filename=" + fileName);
             EasyExcel.write(response.getOutputStream(), clazz)
                     .sheet(sheetName)
-                    .registerWriteHandler(HORIZONTAL_CELL_STYLE_STRATEGY)
                     .doWrite(data);
         } catch (Exception e) {
             log.error("导出Excel异常{}", e.getMessage());
@@ -72,7 +55,6 @@ public class ExcelUtil {
             EasyExcel.write(response.getOutputStream())
                     .withTemplate(templateFileName)
                     .sheet(sheetName)
-                    .registerWriteHandler(HORIZONTAL_CELL_STYLE_STRATEGY)
                     .doWrite(data);
         } catch (Exception e) {
             log.error("导出Excel异常{}", e.getMessage());
