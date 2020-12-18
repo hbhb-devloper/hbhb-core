38fed1a5dd5cd24f690d409b1202a720c614e3ed
xiaokang <648638730@qq.com>
2020-12-18 16:01:57 +0800

    修改

--- src/main/java/com/hbhb/core/utils/MoneyUtil.java
+++ src/main/java/com/hbhb/core/utils/MoneyUtil.java
@@ -6,11 +6,11 @@ import java.text.DecimalFormat;
 /**
  * @author xiaokang
  */
-public class NumberUtil {
+public class MoneyUtil {
 
     private static final String FORMAT_PATTERN = "###,##0.00";
 
-    public static BigDecimal formatMoney(BigDecimal money) {
+    public static BigDecimal format(BigDecimal money) {
         DecimalFormat df = new DecimalFormat(FORMAT_PATTERN);
         return new BigDecimal(df.format(money));
     }
